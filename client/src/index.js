c0019902c34e44201f4e97c16c880c418b2130d3
Juan Gomez <juancarlos.gm931@gmail.com>
2023-05-18 15:36:08 -0500

    deployd front

--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,8 @@ import store from './redux/store';
 import { Provider } from 'react-redux';
 import axios from 'axios';
 
-axios.defaults.baseURL = 'http://localhost:3001';
+// axios.defaults.baseURL = 'http://localhost:3001';
+axios.defaults.baseURL = 'https://pi-deploy-food-back-production.up.railway.app';
 
 ReactDOM.render(
 	<Provider store={store}>

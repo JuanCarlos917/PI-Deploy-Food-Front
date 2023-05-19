050c16d243d837471a0bcb1c39473df630cadb8a
Juan Gomez <juancarlos.gm931@gmail.com>
2023-05-19 12:55:20 -0500

    Se actualiza con npm upgrade

--- client/src/index.js
+++ client/src/index.js
@@ -10,6 +10,7 @@ import axios from 'axios';
 
 //url local
 // axios.defaults.baseURL = 'http://localhost:3001';
+//url deploy
 axios.defaults.baseURL =
 	'https://pi-deploy-food-back-production.up.railway.app/';
 
